6231aac87ac35d55063fab56868e3bad3a5c8e9d
rdom <r.dzhygadlo@gsi.de>
2017-05-11 18:49:18 +0200

    G4_STORE_TRAJECTORY

--- src/PrtTrackingAction.cxx
+++ src/PrtTrackingAction.cxx
@@ -13,13 +13,13 @@ PrtTrackingAction::PrtTrackingAction()
 }
 
 
-void PrtTrackingAction::PreUserTrackingAction(const G4Track* aTrack)
-{
+void PrtTrackingAction::PreUserTrackingAction(const G4Track* aTrack){
+#ifdef G4_STORE_TRAJECTORY  
   // Require rich trajectory...
   fpTrackingManager->SetTrajectory(new G4RichTrajectory(aTrack));
   // Activate storing of auxiliary points for smoother trajectory...
   static G4IdentityTrajectoryFilter curvedFilter;
   G4TransportationManager::GetTransportationManager()->
     GetPropagatorInField()->SetTrajectoryFilter(&curvedFilter);
-
+#endif
 }
