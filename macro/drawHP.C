af7fa8478c2ad718fe761088134493453709ff4d
rdom <rdom@rdom.rdom>
2019-05-21 14:12:55 +0200

    macro to draw hit pattern

---
+++ macro/drawHP.C
@@ -0,0 +1,25 @@
+#define eic__sim
+#include "../src/PrtHit.h"
+#include "../src/PrtEvent.h"
+#include "../../prttools/datainfo.C"
+#include "../../prttools/prttools.C"
+
+void drawHP(TString infile="../build/hits.root"){
+  
+  if(!prt_init(infile,2,"data/drawHP")) return;
+  for (int ievent=0; ievent<prt_entries; ievent++){
+    prt_nextEvent(ievent,1000);
+    for(PrtHit hit : prt_event->GetHits()){
+      int mcp = hit.GetMcpId();
+      int pix = hit.GetPixelId();
+      int ch = map_mpc[mcp][pix];
+      double time = hit.GetLeadTime();      
+      prt_hdigi[mcp]->Fill(pix/16, pix%16);
+    }
+  }
+
+  prt_drawDigi("",2030,0,0);
+  prt_canvasAdd(prt_cdigi);
+  prt_canvasSave(0);
+}
+
