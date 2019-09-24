502f269a51cf14d7cc14ac5611fefc3cef32817c
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2019-09-24 16:25:30 +0200

    comment output

--- src/PrtPixelSD.cxx
+++ src/PrtPixelSD.cxx
@@ -110,19 +110,17 @@ G4bool PrtPixelSD::ProcessHits(G4Step* step, G4TouchableHistory* hist){
     }
   }
 
-  
-  // information from bar
-  G4int collectionID_bar = fSDM->GetCollectionID("BarHitsCollection");
-  PrtBarHitsCollection* barCol = (PrtBarHitsCollection*)(HCofEvent->GetHC(collectionID_bar));  
-  std::cout<<"==================== barCol->entries() "<<barCol->entries()<<std::endl;
-  G4ThreeVector bmom1;
-  for (G4int i=0;i<barCol->entries();i++){
-    PrtBarHit* phit = (*barCol)[i];
-    G4ThreeVector bmom = phit->GetMom();
-    if(i==0)  bmom1 = phit->GetMom();
-    std::cout<<"bmom "<<bmom<< " "<<bmom.angle(bmom1) <<std::endl;
-    
-  }
+  // // information from bar
+  // G4int collectionID_bar = fSDM->GetCollectionID("BarHitsCollection");
+  // PrtBarHitsCollection* barCol = (PrtBarHitsCollection*)(HCofEvent->GetHC(collectionID_bar));  
+  // std::cout<<"==================== barCol->entries() "<<barCol->entries()<<std::endl;
+  // G4ThreeVector bmom1;
+  // for (G4int i=0;i<barCol->entries();i++){
+  //   PrtBarHit* phit = (*barCol)[i];
+  //   G4ThreeVector bmom = phit->GetMom();
+  //   if(i==0)  bmom1 = phit->GetMom();
+  //   std::cout<<"bmom "<<bmom<< " "<<bmom.angle(bmom1) <<std::endl;    
+  // }
   
   PrtHit hit;
   //hit.SetPrizmId(prizmId);
