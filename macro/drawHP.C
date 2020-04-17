2fabde5b62dd8f9a38061ebffb3dadc1d10d92e1
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2020-04-17 15:05:22 +0200

    test

--- macro/drawHP.C
+++ macro/drawHP.C
@@ -17,7 +17,7 @@ void drawHP(TString infile="../build/hits.root"){
       prt_hdigi[mcp]->Fill(pix/16, pix%16);
       //prt_hdigi[mcp]->Fill(pix%16, pix/16);
     }
-  } 
+  }
 
   auto cdigi = prt_drawDigi(2032); //2031
   prt_canvasAdd(cdigi);
