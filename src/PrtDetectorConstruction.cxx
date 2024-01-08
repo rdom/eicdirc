fac141cb6feffab666bfba96c49540884e08a7b3
rdom <r.dzhygadlo@gsi.de>
2024-01-08 16:19:08 +0100

    change radius

--- src/PrtDetectorConstruction.cxx
+++ src/PrtDetectorConstruction.cxx
@@ -102,7 +102,8 @@ PrtDetectorConstruction::PrtDetectorConstruction() : G4VUserDetectorConstruction
 
   if (fGeomType == 1 || fGeomType == 11) { // ePIC ECCE
     fNBoxes = 12;
-    fRadius = 700 + 0.5 * fBar[0]; // old = 729.6;
+    // fRadius = 700 + 0.5 * fBar[0]; // old = 729.6;
+    fRadius = 770.05;
     fNBar = 10;
     fBar[2] = 1225; // BaBar bars
     fBar[1] = 35;
