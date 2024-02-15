3f2c2fd76267a9cedb81260b8e7a977d764d5aea
rdom <r.dzhygadlo@gsi.de>
2024-02-15 19:58:13 +0100

    fix vertex position for LUT

--- src/PrtPrimaryGeneratorAction.cxx
+++ src/PrtPrimaryGeneratorAction.cxx
@@ -156,7 +156,7 @@ void PrtPrimaryGeneratorAction::GeneratePrimaries(G4Event *anEvent) {
     if (fRun->getEv() == 1) barShift = 0.5 * 35;
     if (fGeomType == 2) barShift = 0;
 
-    fParticleGun->SetParticlePosition(G4ThreeVector(0, barShift, 0.5 * fRadiatorL + 630 - 0.2));
+    fParticleGun->SetParticlePosition(G4ThreeVector(-200, barShift, 0.5 * fRadiatorL + 630 - 0.2));
     G4ThreeVector v(0, 0, -1);
     v.setTheta(acos(G4UniformRand()));
     // v.setTheta(0.3);
